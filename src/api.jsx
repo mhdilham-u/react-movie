2bd87b8473b22496e3dc290ac1ab4535252e6f50
Ilham <ilhamushul@gmail.com>
2024-05-28 14:32:13 +0700

    remove: .env file from git

--- src/api.jsx
+++ src/api.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const getMovieList = async () => {
-    const movie = await axios.get(`${import.meta.env.VITE_BASE_URL}/movie/top_rated?api_key=${import.meta.env.VITE_API_KEY}`)
+    const movie = await axios.get(`${import.meta.env.VITE_BASE_URL}/movie/top_rated?page=1&api_key=${import.meta.env.VITE_API_KEY}`)
     // console.log({movieList: movie})
     // return
     return movie.data.results
 }
 export const searchMovie = async (q) => {
-    const search = await axios.get(`${import.meta.env.VITE_BASE_URL}/search/movie?query=${q}&api_key=${import.meta.env.VITE_API_KEY}`)
+    const search = await axios.get(`${import.meta.env.VITE_BASE_URL}/search/movie?query=${q}&page=1&api_key=${import.meta.env.VITE_API_KEY}`)
     return search.data
 }
