7e63f049c01a714460661150c725234c19f6fbb2
Ilham <ilhamushul@gmail.com>
2024-05-28 14:23:52 +0700

    added: movie search feature

--- src/App.jsx
+++ src/App.jsx
@@ -13,9 +13,9 @@ const App = () => {
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => {
       return (
-        <div key={i} className="card flex justify-center items-center flex-col px-8 w-80 h-[550px] bg-slate-200 border-2 border-slate-300 rounded-lg mx-2 mb-3">
-          <div className="movie-title text-2xl font-bold my-4">{movie.title}</div>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} className="w-64 rounded-md" />
+        <div key={i} className="card flex justify-center items-center flex-col px-8 w-80 bg-slate-200 border-2 border-slate-300 rounded-lg mx-2 mb-3">
+          <div className="movie-title text-2xl font-bold my-4 text-center">{movie.title}</div>
+          <img src={`${import.meta.env.VITE_BASE_IMG_URL}/${movie.poster_path}`} className="w-64 rounded-md" />
           <div className="movie-date py-2">{movie.release_date}</div>
           <div className="movie-rating py-2">{movie.vote_average}</div>
         </div>
@@ -23,15 +23,18 @@ const App = () => {
     })
   }
 
-  const search = (q) => {
-    console.log(q);
+  const search = async (q) => {
+    if(q.length > 3) {
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
   }
 
   return (
     <>
       <header>
         <h1 className="text-center text-6xl py-5 font-bold">Movie App</h1>
-          <input type="text" placeholder="Cari movie" className="Movie-search block m-auto my-5" onChange={(target) => search(target.value)} />
+          <input type="text" placeholder="Search movie..." className="Movie-search block m-auto my-5 bg-slate-300 px-5 py-2 rounded" onChange={({target}) => search(target.value)} />
         <div className="flex justify-center flex-wrap m-auto">
           <PopularMovieList />
         </div>
--- src/api.jsx
+++ src/api.jsx
@@ -1,13 +1,12 @@
 import axios from "axios";
 
 export const getMovieList = async () => {
-    const movie = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=91066d075c180b9da912a79df5f20f80
-    `)
+    const movie = await axios.get(`${import.meta.env.VITE_BASE_URL}/movie/top_rated?api_key=${import.meta.env.VITE_API_KEY}`)
     // console.log({movieList: movie})
     // return
     return movie.data.results
 }
 export const searchMovie = async (q) => {
-    const search = await axios.get(q)
-    return
+    const search = await axios.get(`${import.meta.env.VITE_BASE_URL}/search/movie?query=${q}&api_key=${import.meta.env.VITE_API_KEY}`)
+    return search.data
 }
